3efedb37b86cad1b44ab63b082c784f5956455c1
Rayhan Kabir <rayhan@Rayhans-MacBook-Air.local>
2022-11-22 23:52:22 +0600

    registration & login created

--- HomepageApp/forms.py
+++ HomepageApp/forms.py
@@ -1,68 +1,51 @@
 from logging import PlaceHolder
 from django import forms
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, SetPasswordForm, PasswordChangeForm, PasswordResetForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, SetPasswordForm, PasswordChangeForm, PasswordResetForm, UsernameField
 from django.utils.translation import gettext, gettext_lazy as _
-from .models import Vehicle, Users, Garage, Reviews, Rentals
+from .models import Vehicle,Garage, Reviews, Rentals
+from django.contrib.auth.models import User
 from django.contrib.auth import get_user_model
 from django.contrib.auth import password_validation
 
-# vehicleType = (
-#     ('Car','Car'),
-#     ('Mini Bus','Mini Bus'),
-#     ('Bike','Bike'),
-#     ('Bicycle','Bicycle'),
-#     ('Bus','Bus'),
-#     ('Truck','Truck')
-# )
-# # class RegistrationForm(UserCreationForm):
-# #     name = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control', 'PlaceHolder':'Name', 'style':'opacity:0.7;'}))
-# #     email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={'class':'form-control','placeholder':'Email', 'style':'opacity:0.7'}))
-# #     mobilenumber = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'MobileNumber', 'style':'opacity:0.7'}))
-# #     password1 = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class':'form-control','placeholder':'Password', 'style':'opacity:0.7'}))
-# #     password2 = forms.CharField(label='Confirm Password', widget=forms.PasswordInput(attrs={'class':'form-control','placeholder':'Confirm Password', 'style':'opacity:0.7'}))
-# #     class Meta:
-# #         model = User
-# #         fields =['name', 'email', 'mobilenumber', 'password1', 'password2']
+vehicleType = (
+    ('Car','Car'),
+    ('Mini Bus','Mini Bus'),
+    ('Bike','Bike'),
+    ('Bicycle','Bicycle'),
+    ('Bus','Bus'),
+    ('Truck','Truck')
+)
+class RegistrationForm(UserCreationForm):
+        email = forms.CharField(label="Email", required=True, widget=forms.EmailInput(attrs={'class':'form-control','placeholder':'Email', 'style':'opacity:0.8'}))
+        username = forms.CharField(label="Username", widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Username', 'style':'opacity:0.8'}))
+        password1 = forms.CharField(label="Password", widget=forms.PasswordInput(attrs={'class':'form-control','placeholder':'Password', 'style':'opacity:0.8'}))
+        password2 = forms.CharField(label="Confirm Password", widget=forms.PasswordInput(attrs={'class':'form-control','placeholder':'Confirm Password', 'style':'opacity:0.8'}))
+        class Meta:
+            model = User
+            fields = ['username', 'email', 'password1', 'password2']
 
-#   widgets = {'vehicle':forms.TextInput(attrs={'class':'form-control'}),
-#                     'garage':forms.TextInput(attrs={'class':'form-control'}),
-#                     'policy':forms.Select(attrs={'class':'regDropDown', 'style':'color:black;'}),
-#                     }
+class LoginForm(AuthenticationForm):
+    username = UsernameField(widget=forms.TextInput(attrs={'class':'form-control', 'PlaceHolder':'Username', 'style':'opacity:0.8; color:black'}))
+    password = forms.CharField(label=_("Password"), strip=False, widget=forms.PasswordInput(attrs={'autocomplete':'current-password',
+    'class':'form-control', 'PlaceHolder':'Password', 'style':'opacity:0.8;'}))
 
-class RegistrationForm(forms.ModelForm):
-    class Meta:
-        model = Users
-        fields = "__all__"
-        widgets = {
-            'email': forms.EmailInput(attrs={'class':'form-control','placeholder':'Email', 'style':'opacity:0.7'}),
-            'username' : forms.TextInput(attrs={'class':'form-control','placeholder':'Username', 'style':'opacity:0.7'}),
-            'number' : forms.TextInput(attrs={'class':'form-control','placeholder':'PhoneNumber', 'style':'opacity:0.7'}),
-            'password' : forms.PasswordInput(attrs={'class':'form-control','placeholder':'Password', 'style':'opacity:0.7'}),
-            }
-    
+class MyPasswordChangeForm(PasswordChangeForm):
+    old_password = forms.CharField(label=_("Old Password"), strip=False, widget=forms.PasswordInput(attrs={'autocomplete':'current-password',
+    'autofocus':True,'class':'form-control'}))
+    new_password1 = forms.CharField(label=_("New Password"), strip=False, widget=forms.PasswordInput(attrs={'autocomplete':'new-password',
+    'class':'form-control'}),help_text = password_validation.password_validators_help_text_html())
+    new_password2 = forms.CharField(label=_("Confirm New Password"), strip=False, widget=forms.PasswordInput(attrs={'autocomplete':'new-password',
+    'class':'form-control'}))
 
-# # class LoginForm(AuthenticationForm):
-# #     username = forms.EmailField(required=True, widget=forms.EmailInput(attrs={'class':'form-control', 'PlaceHolder':'Email', 'style':'opacity:0.7; color:black'}))
-# #     password = forms.CharField(label=_("Password"), strip=False, widget=forms.PasswordInput(attrs={'autocomplete':'current-password',
-# #     'class':'form-control', 'PlaceHolder':'Password', 'style':'opacity:0.7;'}))
+class MyPasswordResetForm(PasswordResetForm):
+    email = forms.EmailField(label=_("Email"), max_length=254, widget=forms.EmailInput(attrs={'autocomplete':'email',
+    'class':'form-control'}))
 
-# class MyPasswordChangeForm(PasswordChangeForm):
-#     old_password = forms.CharField(label=_("Old Password"), strip=False, widget=forms.PasswordInput(attrs={'autocomplete':'current-password',
-#     'autofocus':True,'class':'form-control'}))
-#     new_password1 = forms.CharField(label=_("New Password"), strip=False, widget=forms.PasswordInput(attrs={'autocomplete':'new-password',
-#     'class':'form-control'}),help_text = password_validation.password_validators_help_text_html())
-#     new_password2 = forms.CharField(label=_("Confirm New Password"), strip=False, widget=forms.PasswordInput(attrs={'autocomplete':'new-password',
-#     'class':'form-control'}))
-
-# class MyPasswordResetForm(PasswordResetForm):
-#     email = forms.EmailField(label=_("Email"), max_length=254, widget=forms.EmailInput(attrs={'autocomplete':'email',
-#     'class':'form-control'}))
-
-# class MySetPasswordForm(SetPasswordForm):
-#     new_password1 = forms.CharField(label=_("New Password"), strip=False, widget=forms.PasswordInput(attrs={'autocomplete':'new-password',
-#     'class':'form-control'}),help_text = password_validation.password_validators_help_text_html())
-#     new_password2 = forms.CharField(label=_("Confirm New Password"), strip=False, widget=forms.PasswordInput(attrs={'autocomplete':'new-password',
-#     'class':'form-control'}))
+class MySetPasswordForm(SetPasswordForm):
+    new_password1 = forms.CharField(label=_("New Password"), strip=False, widget=forms.PasswordInput(attrs={'autocomplete':'new-password',
+    'class':'form-control'}),help_text = password_validation.password_validators_help_text_html())
+    new_password2 = forms.CharField(label=_("Confirm New Password"), strip=False, widget=forms.PasswordInput(attrs={'autocomplete':'new-password',
+    'class':'form-control'}))
 
 # class GarageForm(forms.ModelForm):
 #     class Meta:
@@ -77,14 +60,14 @@ class RegistrationForm(forms.ModelForm):
 #                     'upload':forms.FileInput(attrs={'class':'form-control'}),
 # }
 
-# class VehicleForm(forms.ModelForm):
-#     class Meta:
-#         model = Vehicle
-#         fields = ['name','type', 'vehiclenum']
-#         widgets = {'name':forms.TextInput(attrs={'class':'form-control'}),
-#                     'vehiclenum':forms.TextInput(attrs={'class':'form-control'}),
-#                     'vehicletype':forms.Select(attrs={'class':'regDropDown'})
-#                     }
+class VehicleForm(forms.ModelForm):
+    class Meta:
+        model = Vehicle
+        fields = ['type', 'vehicle_num']
+        widgets = {
+                    'vehicle_num':forms.TextInput(attrs={'class':'form-control'}),
+                    'type':forms.Select(attrs={'class':'regDropDown'})
+                    }
 
 # class ReviewsForm(forms.ModelForm):
 #     class Meta:
--- HomepageApp/views.py
+++ HomepageApp/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render, HttpResponse
-from .forms import RegistrationForm
+from django.shortcuts import render, HttpResponse, redirect
+from .forms import RegistrationForm, VehicleForm, LoginForm
 # from .forms import RegistrationForm, GarageForm, VehicleForm, RentalForm, ReviewsForm, Logform
 # from .models import User, Garage, Reviews, Vehicle, rentals, Customer
 # from .forms import MyPasswordChangeForm, MyPasswordResetForm, MySetPasswordForm
+from django.contrib.auth.forms import AuthenticationForm
 from django.views import View
 from django.contrib import messages
 from django.contrib.auth import authenticate, login
@@ -33,6 +34,25 @@ class RegistrationView(View):
             except:
                 messages.success(request, 'Sorry!! Could not be Registered, Try Again')
         return render(request, 'Homepage/register.html' , {'form':form})
+        
+
+def LoginView(request):
+    # if request.user.is_authenticated:
+    #     return render(request, 'Homeapp/home.html')
+    if request.method == "POST":
+        email = request.POST['email']
+        password = request.POST['password']
+        user = authenticate(request, email=email, password=password)
+        if user is not None:
+            login(request, user)
+            return render(request, 'Homeapp/home.html',{'user':user})
+        else:
+            messages.success(request, 'Something went wrong, Try Again')
+            form = LoginForm()
+            return render(request, 'Homepage/login.html', {'form':form, 'message':messages})
+    else:
+        form = LoginForm()
+        return render(request, 'Homepage/login.html', {'form':form})
 
 
 # class RentalView(View):
@@ -57,16 +77,16 @@ class RegistrationView(View):
 #             messages.success(request, 'Congratulations!! Successfully Registered')
 #         return render(request, 'Homepage/review.html' , {'form':form})
 
-# class VehicleView(View):
-#     def get(self,request):
-#         form = VehicleForm()
-#         return render(request, 'Homepage/vehicle.html' , {'form':form})
-#     def post(self, request):
-#         form = VehicleForm(request.POST)
-#         if form.is_valid():
-#             form.save()
-#             messages.success(request, 'Congratulations!! Successfully Registered')
-#         return render(request, 'Homepage/vehicle.html' , {'form':form})
+class VehicleView(View):
+    def get(self,request):
+        form = VehicleForm()
+        return render(request, 'Homepage/vehicle.html' , {'form':form})
+    def post(self, request):
+        form = VehicleForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Congratulations!! Successfully Registered')
+        return render(request, 'Homepage/vehicle.html' , {'form':form})
 
 # class GarageView(View):
 #     def get(self,request):
